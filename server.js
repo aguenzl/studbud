4a4da3b47a4534a875d6cbdb63c443a658e40c12
aguenzl <ariella.guenzl@gmail.com>
2022-04-29 12:44:25 +1000

    commit week 9 changes

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
    })
 
-let server = app.listen(8888, function(){
-    console.log("App server is running on port 8888");
+let server = app.listen(1234, function(){
+    console.log("App server is running on port 1234");
    });
